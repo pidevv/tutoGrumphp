8d70858e592effd00fdd6ac4ebee1abde3047414
pidevv <pidev@smile.fr>
2020-02-18 13:02:11 +0000

    bad code

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        echo 'bad code';;
+        var_dump($request);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
