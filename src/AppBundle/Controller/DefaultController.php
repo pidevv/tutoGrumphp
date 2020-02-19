4b84a8b078eceeaed126f461167cc585d414417c
pidev <pidev@smile.fr>
2020-02-19 12:02:22 +0100

    add config phpstan

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -10,7 +11,7 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(): Response
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
---
+++ src/AppBundle/Manager/TutoSf2Manager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Manager;
+
+/**
+ * Class TutoSf2Manager.
+ */
+class TutoSf2Manager
+{
+    public function showTuto(): bool
+    {
+        return true;
+    }
+}
